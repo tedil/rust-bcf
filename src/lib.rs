d1c00436574bb28ddf3e0a38e4c0835c617c7324
Till Hartmann <till.hartmann@udo.edu>
2021-05-12 12:22:16 +0200

    add tests for id, ref and alt allele(s)

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,8 @@ pub use record::BcfRecord;
 
 #[cfg(test)]
 mod test {
-
     use super::reader::BcfRecords;
+    use crate::record::Record;
 
     #[test]
     fn test_samples() {
@@ -18,5 +18,29 @@ mod test {
             records.header().samples,
             vec!["HG001", "INTEGRATION", "HG003"]
         );
+    }
+
+    #[test]
+    fn test_id() {
+        let mut records = BcfRecords::from_path("resources/example.id.bcf").unwrap();
+        records
+            .next()
+            .map(|record| assert_eq!(record.id(), b"TestId123"));
+    }
+
+    #[test]
+    fn test_ref_allele() {
+        let mut records = BcfRecords::from_path("resources/example.id.bcf").unwrap();
+        records
+            .next()
+            .map(|record| assert_eq!(record.ref_allele(), b"G"));
+    }
+
+    #[test]
+    fn test_alt_alleles() {
+        let mut records = BcfRecords::from_path("resources/example.id.bcf").unwrap();
+        records
+            .next()
+            .map(|record| assert_eq!(record.alt_alleles(), vec![b"A"]));
     }
 }
